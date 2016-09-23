ab6aa00fa7882917af025a789501da2ce997b9f5
Ole Gunnar Nedrebø <olendr@gmail.com>
2016-09-23 14:43:28 +0200

    before table

--- src/assets/js/components/site.vue
+++ src/assets/js/components/site.vue
@@ -1,9 +1,9 @@
 <template>
-    <span>{{ site.Site.Name }} </span>
-    <span class="{{ getStatusClass() }}">Status: {{ site.Status }} </span>
-    <span>Responsetime: {{ getResponseTime() }}ms </span>
-    <span>Updated: {{ site.Updated }} </span>
-    <span>Size: {{ site.Size }}KB </span>
+    <h1 class="title">{{ site.Site.Name }}</h1>
+    <p>Status: <span class="label {{ getStatusClass() }}">{{ site.Status }}</span> </p>
+    <p>Responsetime: <span class="label {{ getResponseTimeClass() }}">{{ getResponseTime() }}ms</span> </p>
+    <p>Size: <span>{{ site.Size }}KB</span> </p>
+    <p>Updated: <span>{{ site.Updated }}</span> </p>
 </template>
 
 <script>
@@ -20,6 +20,15 @@
                     default:
                         return 'error'
                 }
+            },
+            getResponseTimeClass: function () {
+                let time = this.getResponseTime()
+                if (time < 500) {
+                    return 'success'
+                } else if (time < 2000) {
+                    return 'warning'
+                }
+                return 'error'
             }
         }
     }
