f3d250a39709dd58325094d614aefb69408afc41
Simen Berge <bergesimen@gmail.com>
2016-09-27 10:32:51 +0200

    working on phantomjs

--- static/assets/js/screenshots.js
+++ static/assets/js/screenshots.js
@@ -1,28 +1,50 @@
-var page = require('webpage').create();
 var fs = require('fs');
 
-var stream = fs.open('./sites.json', 'r');
+var page = require('webpage').create();
+page.settings.javascriptEnabled = false;
+page.viewportSize = {
+  width: 1900,
+  height: 1200
+};
+
+fs.changeWorkingDirectory('../../../');
+var stream = fs.open('sites.json', 'r');
 var json = '';
 while(!stream.atEnd()) {
-  json += stream.readLine(); 
+  json += stream.readLine();
 }
 stream.close();
+fs.changeWorkingDirectory('static/assets/files/');
 
 var j = JSON.parse(json);
+var URLS = [];
 for(var i=0; i<j.length; i++) {
-  var name = j[i].name;
   var url = j[i].url;
-  renderImage(name, url);
+  URLS.push(url);
 }
 
+var index = 1;
+function handle_page(file) {
+  console.log(file);
+  page.open(file, function() {
+    page.clipRect = {
+      top: 0,
+      left: 0,
+      width: 1900,
+      height: 1200
+    };
 
-function renderImage(name, url) {  
-  page.viewportSize = {
-      width: 800,
-      height: 600
-  };
-  
-  page.open(url, function() {
-    page.render('../img/' + name + '.png');
+    page.render(index++ + '.png');
+    setTimeout(next_page, 5000);
   });
 }
+
+function next_page() {
+  var file = URLS.shift();
+  if(!file) {
+    phantom.exit(0);
+  }
+  handle_page(file);
+}
+
+next_page();
